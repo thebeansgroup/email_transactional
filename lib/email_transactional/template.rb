cccaee44566856385e2d36cf22bdcbb004348004
Radu Busuioc <ra.busuioc@gmail.com>
2016-02-11 16:16:19 +0000

    added array params fixes  #10

--- lib/email_transactional/template.rb
+++ lib/email_transactional/template.rb
@@ -13,10 +13,42 @@ module EmailTransactional
 
     def bindings_from(params)
       bindings = binding
-      params.each do |key, value|
+      normalized_params(params).each do |key, value|
         bindings.local_variable_set(key.to_sym, value)
       end
       bindings
+    end
+
+    def normalized_params(params)
+      normalized = {}
+      params.each do |key, value|
+        if value.kind_of?(Array)
+          normalized.merge!(normalize_array(key, value))
+        else
+          normalized[key] = value
+        end
+      end
+      normalized
+    end
+
+    def normalize_array(key, array)
+      result = {}
+      array.each_with_index do |element, index|
+        if element.kind_of?(Hash)
+          result.merge!(normalize_hash(key, element, index))
+        else
+          result["#{key}_#{index + 1}"] = element
+        end
+      end
+      result
+    end
+
+    def normalize_hash(root, hash, index)
+      params = {}
+      hash.each do |key, value|
+        params["#{root}_#{key}_#{index + 1}"] = value
+      end
+      params
     end
   end
 end
